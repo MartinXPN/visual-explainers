e24cfbadc9a841f13a8cbfdf20438dbfbc5e3cf1
MartinXPN <mirakyanmartin@gmail.com>
2023-05-11 23:43:45 +0400

    Adjusted times to match the video

--- sliding_window/main.py
+++ sliding_window/main.py
@@ -17,37 +17,55 @@ class OpeningScene(Scene):
         array_mobj = array.get_mobject().center()
         array_name = Text('a:').scale(0.8).move_to(array_mobj, LEFT).shift(0.7 * LEFT)
         self.play(Create(array_mobj), Write(array_name), run_time=2)
-        self.wait(2)
-        self.play(Write(title), Write(k_name), run_time=2)
-        self.wait(3)
+        self.wait()
+        self.play(Write(title), Write(k_name))
+        self.wait()
 
         # Take the range [0, 4] as an example
-        start = 0
         brace = Brace(array_mobj, DOWN, stroke_width=2, color=RED).scale(k / len(a)).align_to(array_mobj, LEFT)
-        sum_text = always_redraw(
-            lambda: Tex(sum(a[start: start + k]), color=RED).scale(0.8).next_to(brace, DOWN, buff=0.2)
+        sum_text = Tex(sum(a[0: k]), color=RED).scale(0.8).next_to(brace, DOWN, buff=0.2)
+        array.highlight(0, k - 1, color=RED)
+        self.play(
+            array_mobj.animate.become(array.get_mobject(), match_center=True),
+            GrowFromCenter(brace),
+            Write(sum_text),
+            run_time=0.5,
         )
-        array.highlight(start, start + k - 1, color=RED)
-        array_mobj.become(array.get_mobject(), match_center=True)
-        self.add(brace, sum_text)
-        self.wait(0.2)
+        self.wait(0.5)
 
-        def highlight(new_start, shift):
-            nonlocal start
+        def highlight(start, additional_mobj=None, shift=0):
             array.unhighlight()
-            array.highlight(new_start, new_start + k - 1, color=RED)
-            array_mobj.become(array.get_mobject(), match_center=True)
-            self.play(brace.animate.shift((array.width + array.spacing) * RIGHT * shift / 2), run_time=0.1)
-            start = new_start
-            self.play(brace.animate.shift((array.width + array.spacing) * RIGHT * shift / 2), run_time=0.1)
-            self.wait(0.2)
+            array.highlight(start, start + k - 1, color=RED)
+            end = start + k - 1
+            if additional_mobj is None:
+                # Create a dummy variable
+                additional_mobj = VGroup()
 
-        highlight(new_start=2, shift=2)     # Take the range [2, 6] as an example
-        highlight(new_start=5, shift=3)     # Take the range [5, 9] as an example
-        highlight(new_start=4, shift=-1)    # Take the range [4, 8] as an example
+            self.play(
+                array_mobj.animate.become(array.get_mobject(), match_center=True),
+                brace.animate.become(
+                    Brace(array_mobj, DOWN, stroke_width=len(array) / max(end - start + 1, 2), color=RED)
+                    .scale((end - start + 1) / len(array))
+                    .align_to(array_mobj, LEFT)
+                    .shift((array.width + array.spacing) * RIGHT * start)
+                ),
+                sum_text.animate.become(
+                    Tex(sum(a[start: end + 1]), color=RED).scale(0.8).next_to(brace, DOWN, buff=0.2)
+                    .align_to(array_mobj, LEFT)
+                    .shift((array.width + array.spacing) * RIGHT * (start + (end - start + 0.75) / 2))
+                ),
+                additional_mobj.animate.shift(shift),
+                run_time=0.3,
+            )
+            self.wait(0.5)
+
+        highlight(start=2)      # Take the range [2, 6] as an example
+        highlight(start=5)      # Take the range [5, 9] as an example
+        highlight(start=4)      # Take the range [4, 8] as an example
+        self.wait(6)
 
         # Add an arrow at location 4 and iterate up to the end
-        highlight(new_start=0, shift=-4)
+        highlight(start=0)
         arrow = Arrow(
             start=UP, end=DOWN, color=RED, buff=0.1,
             stroke_width=10, max_stroke_width_to_length_ratio=15,
@@ -55,13 +73,12 @@ class OpeningScene(Scene):
         ).scale(0.3).next_to(array_mobj, UP).shift((array.width + array.spacing) * 1 * LEFT)
         self.play(Create(arrow))
         for end in range(5, len(array)):
-            self.play(arrow.animate.shift((array.width + array.spacing) * RIGHT), run_time=0.1)
-            highlight(new_start=end - k + 1, shift=1)
-        self.wait(0.2)
+            highlight(start=end - k + 1, additional_mobj=arrow, shift=(array.width + array.spacing) * RIGHT)
+            self.wait(0.2)
+        self.wait(3)
 
         # Move to start
-        self.play(arrow.animate.shift((array.width + array.spacing) * 6 * LEFT), run_time=0.1)
-        highlight(new_start=0, shift=-6)
+        highlight(start=0, additional_mobj=arrow, shift=(array.width + array.spacing) * 6 * LEFT)
         self.wait(0.1)
 
         # Iterate from 4 to the end and display the sum of each subarray
@@ -89,31 +106,30 @@ class OpeningScene(Scene):
                 if formula is not None:
                     self.play(formula[2 + 2 * (i - end + k)].animate.set_color(YELLOW), run_time=0.1)
                     self.play(formula[3 + 2 * (i - end + k)].animate.set_color(YELLOW), run_time=0.1)
-                self.wait(0.1)
+                self.wait(0.2)
             array.highlight(end - 1, end - 1, color=RED)
             array_mobj.become(array.get_mobject(), match_center=True)
             if formula is not None:
                 self.play(formula[11].animate.set_color(YELLOW), run_time=0.1)
                 self.play(formula[12].animate.set_color(YELLOW), run_time=0.1)
+            highlight(start=end - k + 1, additional_mobj=arrow, shift=(array.width + array.spacing) * RIGHT)
 
-            self.play(arrow.animate.shift((array.width + array.spacing) * RIGHT), run_time=0.1)
-            highlight(new_start=end - k + 1, shift=1)
-
-        self.wait(0.1)
-        self.play(Circumscribe(VGroup(*sums[1:]), run_time=0.3))
-        self.wait(0.1)
-
-        self.play(FadeOut(VGroup(*sums)), run_time=0.1)
-        complexity = Tex('Time Complexity: $\\mathcal{O}(n \\cdot k)$', color=RED).next_to(array_mobj, DOWN, buff=1)
-        self.play(Write(complexity), run_time=0.1)
-        self.wait(0.1)
+        self.play(Circumscribe(VGroup(*sums[1:]), run_time=1.5))
+        self.play(FadeOut(VGroup(*sums)), run_time=0.2)
+        complexity = Tex('Time Complexity: $\\mathcal{O}(n \\cdot k)$', color=RED).next_to(array_mobj, DOWN, buff=2)
+        self.play(Write(complexity))
+        self.wait(5)
 
         # Move to next scene
-        self.play(FadeOut(complexity, arrow, brace, sum_text), run_time=0.1)
-        self.wait(0.1)
+        array.unhighlight()
+        self.play(
+            FadeOut(complexity, arrow, brace, sum_text),
+            array_mobj.animate.become(array.get_mobject(), match_center=True),
+        )
+        self.wait(3)
 
 
-REMOVED_COLOR = '#f7dcdd'
+REMOVED_COLOR = '#ffeeee'
 
 
 class SlidingWindowDiscovery(Scene):
@@ -134,81 +150,98 @@ class SlidingWindowDiscovery(Scene):
             max_tip_length_to_length_ratio=0.5, tip_length=0.2,
         ).scale(0.3).next_to(array_mobj, UP).shift((array.width + array.spacing) * 1 * LEFT)
         self.play(Create(arrow))
+        self.wait()
 
-        start = 0
         brace = Brace(array_mobj, DOWN, stroke_width=2, color=RED).scale(k / len(a)).align_to(array_mobj, LEFT)
-        sum_text = always_redraw(
-            lambda: Tex(sum(a[start: start + k]), color=RED).scale(0.8).next_to(brace, DOWN, buff=0.2)
-        )
-        self.add(brace, sum_text)
+        sum_text = Tex(sum(a[: k]), color=RED).scale(0.8).next_to(brace, DOWN, buff=0.2)
+        self.play(GrowFromCenter(brace), Write(sum_text))
+        self.wait(3)
 
-        def highlight(new_start, shift):
-            nonlocal start
+        def highlight(start, additional_mobj=None, shift=0, run_time=0.3, wait=0.5):
             array.unhighlight()
-            array.highlight(0, new_start - 1, color=REMOVED_COLOR)
-            array.highlight(new_start, new_start + k - 1, color=RED)
-            array_mobj.become(array.get_mobject(), match_center=True)
-            self.play(brace.animate.shift((array.width + array.spacing) * RIGHT * shift / 2), run_time=0.1)
-            start = new_start
-            self.play(brace.animate.shift((array.width + array.spacing) * RIGHT * shift / 2), run_time=0.1)
-            self.wait(0.2)
+            array.highlight(start, start + k - 1, color=RED)
+            end = start + k - 1
+            if additional_mobj is None:
+                # Create a dummy variable
+                additional_mobj = VGroup()
+
+            self.play(
+                array_mobj.animate.become(array.get_mobject(), match_center=True),
+                brace.animate.become(
+                    Brace(array_mobj, DOWN, stroke_width=len(array) / max(end - start + 1, 2), color=RED)
+                    .scale((end - start + 1) / len(array))
+                    .align_to(array_mobj, LEFT)
+                    .shift((array.width + array.spacing) * RIGHT * start)
+                ),
+                sum_text.animate.become(
+                    Tex(sum(a[start: end + 1]), color=RED).scale(0.8).next_to(brace, DOWN, buff=0.2)
+                    .align_to(array_mobj, LEFT)
+                    .shift((array.width + array.spacing) * RIGHT * (start + (end - start + 0.75) / 2))
+                ),
+                additional_mobj.animate.shift(shift),
+                run_time=run_time,
+            )
+            self.wait(wait)
 
         for end in range(k, k + 3):
             for i in range(end - k, end):
                 array.highlight(i, i, color=YELLOW, width=8.)
                 if i - 1 >= end - k:
                     array.highlight(i - 1, i - 1, color=RED)
-                array_mobj.become(array.get_mobject(), match_center=True)
-                self.wait(0.1)
+                self.play(array_mobj.animate.become(array.get_mobject(), match_center=True), run_time=0.3)
             array.highlight(end - 1, end - 1, color=RED)
-            array_mobj.become(array.get_mobject(), match_center=True)
+            self.play(array_mobj.animate.become(array.get_mobject(), match_center=True), run_time=0.3)
+            self.wait(1.5)
+            highlight(start=end - k + 1, additional_mobj=arrow, shift=(array.width + array.spacing) * RIGHT)
 
-            self.play(arrow.animate.shift((array.width + array.spacing) * RIGHT), run_time=0.1)
-            highlight(new_start=end - k + 1, shift=1)
-
-        self.play(arrow.animate.shift((array.width + array.spacing) * RIGHT), run_time=0.1)
-
+        self.wait(0.5)
         # Flicker the next element and the one leaving the window
         for i in range(5):
-            array.highlight(k + 3, k + 3, color=RED)
-            array.highlight(3, 3, color=REMOVED_COLOR, width=2.)
-            array_mobj.become(array.get_mobject(), match_center=True)
-            self.wait(0.2)
-            array.highlight(k + 3, k + 3, color=WHITE, width=2.)
-            array.highlight(3, 3, color=RED)
-            array_mobj.become(array.get_mobject(), match_center=True)
-            self.wait(0.2)
+            array.highlight(k + 2, k + 2, color=RED, width=8.)
+            array.highlight(2, 2, color=REMOVED_COLOR, width=2.)
+            self.play(array_mobj.animate.become(array.get_mobject(), match_center=True), run_time=0.4)
+            array.highlight(k + 2, k + 2, color=WHITE, width=2.)
+            array.highlight(2, 2, color=RED, width=8.)
+            self.play(array_mobj.animate.become(array.get_mobject(), match_center=True), run_time=0.4)
 
-        highlight(new_start=4, shift=1)
-        self.wait(0.1)
+        highlight(start=3)
+        self.wait(4)
+        array.highlight(2, 2, color=YELLOW, width=8.)
+        self.play(array_mobj.animate.become(array.get_mobject(), match_center=True), run_time=0.3)
+        self.wait(2)
+        array.highlight(k + 2, k + 2, color=GREEN, width=8.)
+        self.play(array_mobj.animate.become(array.get_mobject(), match_center=True), run_time=0.3)
+        self.wait(4)
 
         # Move back to start
-        self.play(arrow.animate.shift((array.width + array.spacing) * LEFT * 4), run_time=0.1)
-        highlight(new_start=0, shift=-4)
-        self.wait(0.1)
+        highlight(start=0, additional_mobj=arrow, shift=(array.width + array.spacing) * LEFT * 3)
+        self.wait(2)
 
         # Write initial window formula
-        formula = MathTex('\\mathrm{sum}_0 = a_0 + a_1 + ... + a_{k-1}', color=RED).scale(0.8)
-        formula.next_to(array_mobj, DOWN, buff=1)
-        self.play(Write(formula), run_time=0.1)
-        self.wait(0.1)
+        formula = MathTex('\\mathrm{sum}_0 = a_0 + a_1 + ... + a_{k-1}', color=YELLOW).scale(0.8)
+        formula.next_to(array_mobj, DOWN, buff=1.5)
+        self.play(Write(formula))
+        self.wait()
 
         # Move the window once
-        self.play(arrow.animate.shift((array.width + array.spacing) * RIGHT), run_time=0.1)
-        highlight(new_start=1, shift=1)
+        highlight(start=1, additional_mobj=arrow, shift=(array.width + array.spacing) * RIGHT)
+        self.wait(1)
 
         # Formula for any window
-        window = MathTex('\\mathrm{sum}_i = \\mathrm{sum}_{i-1} + a_{r} - a_{r - k}', color=RED).scale(0.8)
+        window = MathTex('\\mathrm{sum}_i = \\mathrm{sum}_{i-1} + a_{r} - a_{r - k}', color=YELLOW).scale(0.8)
         window.next_to(formula, DOWN, buff=0.5).align_to(formula, LEFT)
-        self.play(Write(window), run_time=0.1)
-        self.wait(0.1)
+        self.play(Write(window))
+        self.wait(6)
+        self.play(Circumscribe(window), run_time=2)
+        self.wait(6)
 
-        self.play(FadeOut(formula, window), run_time=0.1)
+        self.play(FadeOut(formula, window))
+        self.wait(6)
 
         # Code for the sliding window
         code = Code(
             code=dedent('''
-                best = cur = sum(a[:k])
+                cur = best = sum(a[:k])
                 for r in range(k, len(a)):
                     cur = cur + a[r] - a[r - k]
                     best = max(best, cur)
@@ -221,27 +254,32 @@ class SlidingWindowDiscovery(Scene):
         ).next_to(array_mobj, DOWN, buff=1).code
         for line in code.chars:
             self.play(AddTextLetterByLetter(line))
-        self.wait(0.1)
+            self.wait(3)
 
         # Move to start
-        self.play(arrow.animate.shift((array.width + array.spacing) * LEFT), run_time=0.1)
-        highlight(new_start=0, shift=-1)
+        self.wait(2)
+        highlight(start=0, additional_mobj=arrow, shift=(array.width + array.spacing) * LEFT)
+        self.wait(4)
 
         # Simulate the algorithm and keep the sum at each location (match the sum center)
         cur = sum_text.copy()
         self.add(cur)
         sums = [cur]
         for end in range(k, len(array)):
-            self.play(arrow.animate.shift((array.width + array.spacing) * RIGHT), run_time=0.1)
-            highlight(new_start=end - k + 1, shift=1)
+            highlight(start=end - k + 1, additional_mobj=arrow, shift=(array.width + array.spacing) * RIGHT,
+                      run_time=0.1 if end > k + 1 else 0.3, wait=0.1 if end > k + 1 else 0.5)
             cur = sum_text.copy()
             self.add(cur)
             sums.append(cur)
-            self.wait(0.1)
+            if end <= k + 1:
+                self.wait(1)
 
-        self.play(FadeOut(arrow, brace, sum_text), run_time=0.1)
-        self.play(Circumscribe(VGroup(*sums)), run_time=0.3)
-        self.wait(0.1)
+        self.play(FadeOut(arrow, brace, sum_text))
+        self.play(Circumscribe(VGroup(*sums)), run_time=3)
+
+        # Indicate the first element in the sum (the final answer)
+        self.play(Indicate(sums[0]), run_time=4)
+        self.wait(2)
 
 
 class DynamicSizeSlidingWindow(Scene):
