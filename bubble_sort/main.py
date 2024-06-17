22efe10b4158b62083ff541e691f66924f619454
MartinXPN <mirakyanmartin@gmail.com>
2024-06-17 18:58:29 +0400

    Added the Optimization2 scene

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -691,3 +691,163 @@ class Optimization1(Scene):
         code.become(optimized_code)
         self.play(ApplyWave(code, run_time=0.5))
 
+
+class Optimization2(Scene):
+    def construct(self):
+        title = Title('Bubble Sort Implementation', include_underline=False)
+        self.add(title)
+
+        array = Array([3, 5, 4, 1, 7, 9, 12], color=BLACK, cell_type='bubble')
+        array_mobj = array.get_mobject().center().shift(1.5 * UP)
+        a_text = Tex('a:').scale(0.9).next_to(array_mobj, LEFT)
+
+        # Highlight (without any animation) the last 2 cells and make them green
+        darker = interpolate_color(GREEN, BLACK, 0.1)
+        for rect in array.cells[-2:]:
+            rect[1].set_fill(color=GREEN)
+            rect[3].set_fill(color=darker)
+
+        indices = Array(
+            [i for i in range(len(array))],
+            width=array.width, height=array.height,
+            spacing=array.spacing, scale_text=array.scale_text, stroke_color=BLACK,
+        )
+        indices_mobj = indices.get_mobject().center().next_to(array_mobj, 0.1 * UP)
+        self.add(a_text, array_mobj, indices_mobj)
+
+        code = Code(
+            code=dedent('''
+                for u in range(len(a) - 1, 0, -1):
+                    for i in range(u):
+                        if a[i] > a[i + 1]:
+                            a[i], a[i + 1] = a[i + 1], a[i]
+            ''').strip(),
+            tab_width=4,
+            language='Python',
+            line_spacing=0.6,
+            font='Monospace',
+            style='monokai',
+        ).next_to(array_mobj, DOWN).shift(0.3 * RIGHT).code
+        self.add(code)
+
+        # Run bubble-sort animation for 1 sweep
+        def sweep(run_times: list):
+            nonlocal array, array_mobj
+            for i, time in enumerate(run_times):
+                self.play(*highlight(array, i, i + 2, ORANGE, 5), run_time=time)
+                self.wait(time)
+                if array.values[i] > array.values[i + 1]:
+                    new_array, new_array_mobj = swap(array, array_mobj, i, i + 1, aligned_edge=RIGHT if i == 0 else LEFT)
+                    self.play(TransformMatchingCells(array_mobj, new_array_mobj, path_arc=PI/3), run_time=time * 7)
+                    self.wait(time)
+                    array, array_mobj = new_array, new_array_mobj
+                else:
+                    self.wait(2 * time)
+
+                self.play(*highlight(array, i, i + 1, BLUE_BACKGROUND, 0), run_time=time / 10)
+            self.play(*highlight(array, 0, len(run_times) + 1, BLUE_BACKGROUND, 0), run_time=0.1)
+
+        sweep([0.04, 0.04, 0.04, 0.04])
+        self.play(*highlight(array, 4, 5, GREEN, 5), run_time=0.1)
+
+        self.play(
+            Indicate(array.cells[-1]),
+            Indicate(array.cells[-2]),
+            Indicate(array.cells[-3]),
+            Indicate(array.cells[-4]),
+            run_time=0.5
+        )
+        self.play(Indicate(code.chars[0], run_time=0.5))
+
+        sweep([0.04, 0.04, 0.04])
+        self.play(*highlight(array, 3, 4, GREEN, 5), run_time=0.1)
+        sweep([0.04])
+        self.play(*highlight(array, 2, 3, GREEN, 5), run_time=0.1)
+
+        optimized_code = Code(
+            code=dedent('''
+                for u in range(len(a) - 1, 0, -1):
+                    changed = False
+                    for i in range(u):
+                        if a[i] > a[i + 1]:
+                            a[i], a[i + 1] = a[i + 1], a[i]
+                            changed = True
+                    if not changed:
+                        break
+            ''').strip(),
+            tab_width=4,
+            language='Python',
+            line_spacing=0.6,
+            font='Monospace',
+            style='monokai',
+        ).next_to(array_mobj, DOWN).shift(0.15 * DOWN).shift(0.25 * RIGHT).code
+
+        self.play(VGroup(*code.chars[1:]).animate.shift(0.6 * DOWN), run_time=0.1)
+        self.wait(0.1)
+        self.play(AddTextLetterByLetter(optimized_code.chars[1], run_time=0.05 * len(optimized_code.chars[1])))
+        self.wait(0.1)
+
+        self.play(AddTextLetterByLetter(optimized_code.chars[5], run_time=0.05 * len(optimized_code.chars[2])))
+        self.wait(0.1)
+
+        # Indicate the inner loop along with its contents
+        self.play(Indicate(VGroup(code.chars[1], code.chars[2], code.chars[3], optimized_code.chars[5]), run_time=0.5))
+
+        self.play(AddTextLetterByLetter(optimized_code.chars[6], run_time=0.05 * len(optimized_code.chars[6])))
+        self.play(AddTextLetterByLetter(optimized_code.chars[7], run_time=0.05 * len(optimized_code.chars[7])))
+        self.wait(0.1)
+
+        for i, time in enumerate([0.04] * len(array)):
+            self.play(*highlight(array, i, i + 2, ORANGE, 5), run_time=time)
+            self.wait(time)
+            self.play(*highlight(array, i, i + 1, BLUE_BACKGROUND, 0), run_time=time / 10)
+
+        self.play(ReplacementTransform(title, Title('Bubble Sort', include_underline=False)), run_time=0.5)
+        self.wait(0.1)
+
+        # Bring the array to the initial state
+        initial_array = Array(a, color=BLACK, cell_type='bubble')
+        initial_array_mobj = initial_array.get_mobject().center().shift(1.5 * UP)
+        self.play(TransformMatchingCells(array_mobj, initial_array_mobj, path_arc=PI/3), run_time=0.5)
+        self.wait(1)
+
+
+class Simulation(Scene):
+    def construct(self):
+        title = Title('Bubble Sort', include_underline=False)
+        self.add(title)
+
+        array = Array(a, color=BLACK, cell_type='bubble')
+        array_mobj = array.get_mobject().center().shift(1.5 * UP)
+        a_text = Tex('a:').scale(0.9).next_to(array_mobj, LEFT)
+
+        indices = Array(
+            [i for i in range(len(array))],
+            width=array.width, height=array.height,
+            spacing=array.spacing, scale_text=array.scale_text, stroke_color=BLACK,
+        )
+        indices_mobj = indices.get_mobject().center().next_to(array_mobj, 0.1 * UP)
+        self.add(a_text, array_mobj, indices_mobj)
+
+        code = Code(
+            code=dedent('''
+                        for u in range(len(a) - 1, 0, -1):
+                            changed = False
+                            for i in range(u):
+                                if a[i] > a[i + 1]:
+                                    a[i], a[i + 1] = a[i + 1], a[i]
+                                    changed = True
+                            if not changed:
+                                break
+                    ''').strip(),
+            tab_width=4,
+            language='Python',
+            line_spacing=0.6,
+            font='Monospace',
+            style='monokai',
+        ).next_to(array_mobj, DOWN).shift(0.15 * DOWN).shift(0.25 * RIGHT).code
+        self.add(code)
+
+        self.play(VGroup(*code.chars).animate.shift(1.5 * LEFT))
+        self.wait(0.1)
+
